82e1a733a13ca495054a1bf21efd0715db212bea
Nemésio Florence Vieira Filho <54808745+NemesioFVF@users.noreply.github.com>
2021-02-02 03:16:05 -0300

    Add files via upload

---
+++ Ex1/Ex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva seu nome: ");
+            String n = Convert.ToString(Console.ReadLine());
+            Console.Write("Escreva sua idade: ");
+            String i = Convert.ToString(Console.ReadLine());
+            Console.Write("Escreva a cidade onde você mora: ");
+            String c = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Nome: " + n);
+            Console.WriteLine("Idade: " + i + " anos");
+            Console.WriteLine("Cidade: " + c);
+            Console.ReadKey();
+        }
+    }
+}
